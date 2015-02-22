b598e744c44ef86df4fdeffd320250eed2e542fd
Laurent <spywen@hotmail.fr>
2015-02-22 23:36:40 +0100

    Externalize script. Need improvements + need manage UI with success case.

--- dev/modules/common/game/rulesService.js
+++ dev/modules/common/game/rulesService.js
@@ -1,6 +1,155 @@
 angular.module('hex7.game.rulesService', [])
-.factory('rulesService', function(){
-	return {
+.constant('gameConstants',{
+	'state':{
+		'default':'default',
+		'player1':'player1',
+		'player2':'player2'
+	},
+	'case':{
+		x: 0,//x
+		y: 0,//y
+		posx:0,//css x position
+		posy:0,//css y position
+		state:"default",//state : default, player1, player2
+		selected:false,
+		alreadyTested:false
+	},
+	"xposFactor":53,
+	"yposFactor":46,
+	"posLineDecalFactor":26
+})
+.factory('rulesService', function(gameConstants){
 
+	return {
+		initBoard: initBoard,
+		checkIfSomeoneWin: checkIfSomeoneWin,
+		gameConstants:gameConstants
 	};
+
+	function initBoard(board, size){
+		var index = 0;
+		var lineDecal = 0;
+		for(var y=0;y<size;y++){
+			for(var x=0; x<size;x++){
+				var newCase = _.clone(gameConstants.case);
+				newCase.x = x;
+				newCase.y = y;
+				newCase.posx = x * gameConstants.xposFactor + lineDecal;
+				newCase.posy = y * gameConstants.yposFactor;
+				if(newCase.x===0 && newCase.y===0){//Just for the first case
+					newCase.selected = true;
+				}
+				board.push(newCase);
+			}
+			lineDecal+=gameConstants.posLineDecalFactor;
+		}
+	}
+
+	function checkIfSomeoneWin(board, size){
+		_.forEach([
+			{//PLAYER 1
+				'where':{'y':0, 'state':gameConstants.state.player1},
+				'side':'y',
+				'state':gameConstants.state.player1
+			},
+			{//PLAYER 2
+				'where':{'x':0, 'state':gameConstants.state.player2},
+				'side':'x',
+				'state':gameConstants.state.player2
+			}
+		], function(player){
+			if(player.side === 'y'){
+				player.where.y = 0;
+			}else{
+				player.where.x = 0;
+			}
+			var playerFirstSidePieces = _.where(board, player.where);
+			if(player.side === 'y'){
+				player.where.y = size-1;
+			}else{
+				player.where.x = size-1;
+			}
+			var playerEndSidePieces = _.where(board, player.where);
+			if(playerFirstSidePieces.length > 0 && playerEndSidePieces.length > 0){
+				var otherPieces = [];
+				_.map(_.where(board, {'state':player.state}), function(piece){
+					if(piece.y>0){
+						otherPieces.push(piece);
+					}
+				});
+				var solutionFound = false;
+				_.forEach(playerFirstSidePieces, function(firstSidePiece){
+					var path = [];
+					path.push(firstSidePiece);
+					var endOfSearch = false;
+					while(!endOfSearch){
+						var newPathFound = false;
+						if(_.where(otherPieces, {'x':_.last(path).x+1, 'y':_.last(path).y, 'alreadyTested':false}).length>0){
+							newPathFound = true;
+							path.push(_.first(_.where(otherPieces, {'x':_.last(path).x+1, 'y':_.last(path).y, 'alreadyTested':false})));
+						}
+						else if(_.where(otherPieces, {'x':_.last(path).x-1, 'y':_.last(path).y, 'alreadyTested':false}).length>0){
+							newPathFound = true;
+							path.push(_.first(_.where(otherPieces, {'x':_.last(path).x-1, 'y':_.last(path).y, 'alreadyTested':false})));
+						}
+						else if(_.where(otherPieces, {'x':_.last(path).x, 'y':_.last(path).y+1, 'alreadyTested':false}).length>0){
+							newPathFound = true;
+							path.push(_.first(_.where(otherPieces, {'x':_.last(path).x, 'y':_.last(path).y+1, 'alreadyTested':false})));
+						}
+						else if(_.where(otherPieces, {'x':_.last(path).x, 'y':_.last(path).y-1, 'alreadyTested':false}).length>0){
+							newPathFound = true;
+							path.push(_.first(_.where(otherPieces, {'x':_.last(path).x, 'y':_.last(path).y-1, 'alreadyTested':false})));
+						}
+						else if(_.where(otherPieces, {'x':_.last(path).x+1, 'y':_.last(path).y-1, 'alreadyTested':false}).length>0){
+							newPathFound = true;
+							path.push(_.first(_.where(otherPieces, {'x':_.last(path).x+1, 'y':_.last(path).y-1, 'alreadyTested':false})));
+						}
+						else if(_.where(otherPieces, {'x':_.last(path).x-1, 'y':_.last(path).y+1, 'alreadyTested':false}).length>0){
+							newPathFound = true;
+							path.push(_.first(_.where(otherPieces, {'x':_.last(path).x-1, 'y':_.last(path).y+1, 'alreadyTested':false})));
+						}
+
+						if(path.length === 0){
+							endOfSearch = true;
+						}else{
+							if(player.side === 'y'){
+								player.where.y = size-1;
+							}else{
+								player.where.x = size-1;
+							}
+							if(_.where(path, player.where).length>0){
+								solutionFound = true;
+								endOfSearch = true;
+							}else{
+								if(!newPathFound){
+									path = _.slice(path,0,path.length-1);
+									if(path.length===0)
+										endOfSearch = true;
+								}else{
+									_.last(path).alreadyTested = true;
+								}
+							}
+						}
+					}
+				});
+				cleanBoard(board);
+				if(solutionFound){
+					console.log('Player 1 win !');
+					return true;
+				}else{
+					return false;
+				}
+			}
+		});
+	}
+
+	function checkIfSomeoneCouldWin(){
+
+	}
+
+	function cleanBoard(board){
+		_.forEach(board, function(c){
+			c.alreadyTested = false;
+		});
+	}
 });
--- dev/modules/gameBoard/gameBoard.js
+++ dev/modules/gameBoard/gameBoard.js
@@ -9,155 +9,54 @@ angular.module('hex7.gameBoard',[
         templateUrl: "gameBoard.html"
     });
 })
-.constant('elementsConstants',{
-	'case':{
-		x: 0,//x
-		y: 0,//y
-		posx:0,//css x position
-		posy:0,//css y position
-		state:"default",//state : default, player1, player2
-		selected:"notSelected",//selected by the cursor or not,
-		alreadyTested:false
-	}
-})
-.controller('gameBoardCtrl', function($scope, $routeParams, $sce, elementsConstants, hotkeys, rulesService){
+.controller('gameBoardCtrl', function($scope, $routeParams, $sce, hotkeys, rulesService){
 	//Global variables
 	$scope.level = $routeParams.level;
 	$scope.size = $routeParams.size;
 
 	//Game variables
-	$scope.boardCases = [];
-	$scope.selectedCase = {x:0,y:0};
+	$scope.board = [];
+	$scope.selectedBox = {x:0,y:0};
 	$scope.turn = true;//true <=> player1, false <=> player2
 	$scope.winner = '';
-	
+
 
 	//Initialisation of the game board
 	$scope.initBoard = function(){
-		var index = 0;
-		var lineDecal = 0;
-		for(var y=0;y<$scope.size;y++){
-			for(var x=0; x<$scope.size;x++){
-				var newCase = _.clone(elementsConstants.case);
-				newCase.x = x;
-				newCase.y = y;
-				newCase.posx = x * 53 + lineDecal;
-				newCase.posy = y * 46;
-				if(newCase.x===0 && newCase.y===0){//Just for the first case
-					newCase.selected = "selected";
-				}
-				$scope.boardCases.push(newCase);
+		rulesService.initBoard($scope.board, $scope.size);
+	};
+
+	//Refresh case selected (old case selected is no more selected and new case selected is selected)
+	$scope.changeOfSelectedBox = function(x,y){
+		//Check the possibility to change of case
+		var couldChangeOfBox = false;
+		if(x !== 0){
+			if($scope.selectedBox.x + x >= 0 && $scope.selectedBox.x + x < $scope.size){
+				couldChangeOfBox = true;
 			}
-			lineDecal+=26;
+		}else{
+			if($scope.selectedBox.y + y >= 0 && $scope.selectedBox.y + y < $scope.size){
+				couldChangeOfBox = true;
+			}
+		}
+		
+		if(couldChangeOfBox){
+			//Unselect old case
+			_.first(_.where($scope.board, {'x':$scope.selectedBox.x, 'y':$scope.selectedBox.y})).selected = false;
+			//Find new case selected
+			$scope.selectedBox.x += x;
+			$scope.selectedBox.y += y;
+			//Select new case
+			_.first(_.where($scope.board, {'x':$scope.selectedBox.x, 'y':$scope.selectedBox.y})).selected = true;
 		}
 	};
 
-	$scope.refreshSelection = function(){
-		_.first(_.where($scope.boardCases, {'selected':'selected'})).selected = 'notSelected';
-		_.first(_.where($scope.boardCases, {'x':$scope.selectedCase.x, 'y':$scope.selectedCase.y})).selected = 'selected';
-	};
-
-	$scope.checkEnd = function(){
-		//Player 1
-		var player1TopPieces = _.where($scope.boardCases, {'y':0, 'state':'player1'});
-		var player1BotPieces = _.where($scope.boardCases, {'y':$scope.size-1, 'state':'player1'});
-		if(player1TopPieces.length > 0 && player1BotPieces.length > 0){
-			console.log('One solution could be found for the player 1');
-			var piecesWithoutTopBorderPieces = [];
-			_.map(_.where($scope.boardCases, {'state':'player1'}), function(piece){
-				if(piece.y>0){
-					piecesWithoutTopBorderPieces.push(piece);
-				}
-			});
-			console.log('Internal pieces found : '+piecesWithoutTopBorderPieces.length);
-			var solutionFound = false;
-			_.forEach(player1TopPieces, function(topPiece){
-				var path = [];
-				path.push(topPiece);
-				var endOfSearch = false;
-				while(!endOfSearch){
-					var newPathFound = false;
-					if(_.where(piecesWithoutTopBorderPieces, {'x':_.last(path).x+1, 'y':_.last(path).y, 'alreadyTested':false}).length>0){
-						newPathFound = true;
-						_.last(path).sol = true;
-						path.push(_.first(_.where(piecesWithoutTopBorderPieces, {'x':_.last(path).x+1, 'y':_.last(path).y, 'alreadyTested':false})));
-					}
-					else if(_.where(piecesWithoutTopBorderPieces, {'x':_.last(path).x-1, 'y':_.last(path).y, 'alreadyTested':false}).length>0){
-						newPathFound = true;
-						_.last(path).sol = true;
-						path.push(_.first(_.where(piecesWithoutTopBorderPieces, {'x':_.last(path).x-1, 'y':_.last(path).y, 'alreadyTested':false})));
-					}
-					else if(_.where(piecesWithoutTopBorderPieces, {'x':_.last(path).x, 'y':_.last(path).y+1, 'alreadyTested':false}).length>0){
-						newPathFound = true;
-						_.last(path).sol = true;
-						path.push(_.first(_.where(piecesWithoutTopBorderPieces, {'x':_.last(path).x, 'y':_.last(path).y+1, 'alreadyTested':false})));
-					}
-					else if(_.where(piecesWithoutTopBorderPieces, {'x':_.last(path).x, 'y':_.last(path).y-1, 'alreadyTested':false}).length>0){
-						newPathFound = true;
-						_.last(path).sol = true;
-						path.push(_.first(_.where(piecesWithoutTopBorderPieces, {'x':_.last(path).x, 'y':_.last(path).y-1, 'alreadyTested':false})));
-					}
-					else if(_.where(piecesWithoutTopBorderPieces, {'x':_.last(path).x+1, 'y':_.last(path).y-1, 'alreadyTested':false}).length>0){
-						newPathFound = true;
-						_.last(path).sol = true;
-						path.push(_.first(_.where(piecesWithoutTopBorderPieces, {'x':_.last(path).x+1, 'y':_.last(path).y-1, 'alreadyTested':false})));
-					}
-					else if(_.where(piecesWithoutTopBorderPieces, {'x':_.last(path).x-1, 'y':_.last(path).y+1, 'alreadyTested':false}).length>0){
-						newPathFound = true;
-						_.last(path).sol = true;
-						path.push(_.first(_.where(piecesWithoutTopBorderPieces, {'x':_.last(path).x-1, 'y':_.last(path).y+1, 'alreadyTested':false})));
-					}
-
-					//Verif que dans le dernier pion ajouté n'appartient pas au bot
-					if(path.length === 0){
-						endOfSearch = true;
-					}else{
-						if(_.where(path, {'y':$scope.size-1}).length>0){
-							console.log('SOLUTION!!!');
-							solutionFound = true;
-							endOfSearch = true;
-						}else{
-							if(!newPathFound){
-								console.log(path.length);
-								path = _.slice(path,0,path.length-1);
-								console.log(path.length);
-								if(path.length===0)
-									endOfSearch = true;
-									//_.last(path).alreadyTested = false;
-							}else{
-								_.last(path).alreadyTested = true;
-							}
-						}
-					}
-				}
-			});
-			$scope.cleanPieceAlreadyTested($scope.boardCases);
-			if(solutionFound){
-				console.log('Player 1 win !');
-			}
-		}
-		//Player 2
-		/*var player2LeftPieces = _.where($scope.boardCases, {'x':0, 'state':'player2'});
-		var player2RightPieces = _.where($scope.boardCases, {'x':$scope.size-1, 'state':'player2'});
-		if(player2LeftPieces.length > 0 && player2RightPieces.length > 0){
-			console.log('One solution could be found for the player 2');
-		}*/
-	};
-
-	$scope.cleanPieceAlreadyTested = function(boardCases){
-		_.forEach(boardCases, function(c){
-			c.alreadyTested = false;
-		});
-	};
-
+	//HotKeys ------------------------------------------------------------------------
 	hotkeys.add({
 		combo: 'z',
 		description: 'go up',
 		callback: function() {
-			if($scope.selectedCase.y - 1 >= 0){
-				$scope.selectedCase.y--;
-			}
-			$scope.refreshSelection();
+			$scope.changeOfSelectedBox(0,-1);
 		}
 	});
 
@@ -165,10 +64,7 @@ angular.module('hex7.gameBoard',[
 		combo: 's',
 		description: 'go down',
 		callback: function() {
-			if($scope.selectedCase.y + 1 < $scope.size){
-				$scope.selectedCase.y++;
-			}
-			$scope.refreshSelection();
+			$scope.changeOfSelectedBox(0,1);
 		}
 	});
 
@@ -176,10 +72,7 @@ angular.module('hex7.gameBoard',[
 		combo: 'q',
 		description: 'go left',
 		callback: function() {
-			if($scope.selectedCase.x - 1 >= 0){
-				$scope.selectedCase.x--;
-			}
-			$scope.refreshSelection();
+			$scope.changeOfSelectedBox(-1,0);
 		}
 	});
 
@@ -187,10 +80,7 @@ angular.module('hex7.gameBoard',[
 		combo: 'd',
 		description: 'go right',
 		callback: function() {
-			if($scope.selectedCase.x + 1 < $scope.size){
-				$scope.selectedCase.x++;
-			}
-			$scope.refreshSelection();
+			$scope.changeOfSelectedBox(1,0);
 		}
 	});
 
@@ -198,10 +88,12 @@ angular.module('hex7.gameBoard',[
 		combo: 'space',
 		description: 'take',
 		callback: function(event) {
-			if(_.first(_.where($scope.boardCases, {'x':$scope.selectedCase.x, 'y':$scope.selectedCase.y})).state === 'default'){
-				_.first(_.where($scope.boardCases, {'x':$scope.selectedCase.x, 'y':$scope.selectedCase.y})).state = $scope.turn ? 'player1' : 'player2';
-				//$scope.turn = !$scope.turn;
-				$scope.checkEnd();
+			if(_.first(_.where($scope.board, {'x':$scope.selectedBox.x, 'y':$scope.selectedBox.y})).state === rulesService.gameConstants.state.default){
+				_.first(_.where($scope.board, {'x':$scope.selectedBox.x, 'y':$scope.selectedBox.y})).state = 
+				$scope.turn ? rulesService.gameConstants.state.player1 : rulesService.gameConstants.state.player2;
+				
+				$scope.turn = !$scope.turn;
+				rulesService.checkIfSomeoneWin($scope.board, $scope.size);
 				event.preventDefault();
 			}
 		}
